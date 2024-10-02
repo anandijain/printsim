8ea9ed0e3164f3382f2e9cb84ab3d3356e3b4807
anand jain <anandj@uchicago.edu>
2024-10-02 15:59:35 -0700

    gpt

--- code.jl
+++ code.jl
@@ -29,20 +29,20 @@ end
 gs = filter(startswith("G1"), readlines("gsq.gcode"))
 function doesit(gs)
     i = 1
-    cur_pos = [0., 0., 0., 0., 0.]
-    last_pos = [0., 0., 0., 0., 0.]
+    cur_pos = [0.0, 0.0, 0.0, 0.0, 0.0]
+    last_pos = [0.0, 0.0, 0.0, 0.0, 0.0]
     all_ps = []
     cur_path = []
 
-
     for g in first(gs, 40)
         as = split(find_first_part(g))[2:end]
-        (cur_pos, efound) = update_cur_pos!(cur_pos, as)#, last_pos[4])
-        # here e is relative still 
+        (cur_pos, efound) = update_cur_pos!(cur_pos, as)
 
-        # @info cur_pos
-        if last_pos[4] + cur_pos[4] > 0
-            cur_pos[4] = 0
+        # Determine if extrusion is happening
+        extruding = (last_pos[4] + cur_pos[4]) > 0
+
+        if extruding
+            # Starting or continuing an extrusion path
             if isempty(cur_path)
                 p = [last_pos, cur_pos]
                 @show "starting a new path" p
@@ -51,28 +51,31 @@ function doesit(gs)
                 @show "adding to existing path" cur_pos, cur_path
                 push!(cur_path, copy(cur_pos))
             end
-        else # e[i-1] + e[i] <= 0 (NEW PATH)
-            # @show 
+        else
+            # No extrusion or finished, check for new path
             if efound
                 cur_pos[4] = last_pos[4] + cur_pos[4]
             end
             if !isempty(cur_path)
-                @show "adding cur path to all_pls" cur_path
-                push!(all_ps, cur_path)
+                @show "adding cur path to all_ps" cur_path
+                push!(all_ps, copy(cur_path))
                 cur_path = []
             end
         end
 
         @info i, (last_pos, cur_pos)
-        cur_pos[4] = min(0, cur_pos[4])
+        cur_pos[4] = min(0, cur_pos[4])  # Reset E if needed
 
         last_pos = copy(cur_pos)
         i += 1
     end
 
-    
-    all_ps
+    # If any remaining path, add to all_ps
+    if !isempty(cur_path)
+        push!(all_ps, copy(cur_path))
+    end
 
+    all_ps
 end
 
 ps = doesit(gs)
